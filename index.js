e06cc7f7c00de2aad782c1d2bd9a8c2d15866645
Mega-Barrel <saurabhjoshi7869@gmail.com>
2021-07-11 17:35:50 +0530

    API connections

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const pool = require("./db")
+
+app.use(express.json()) // => req.body
+
+// ROUTES //
+
+// Login Data
+
+// Register Data
+
+// Answers Submission
+
+// Results
