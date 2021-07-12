603beb515b10bb5e078347b01f35b8c515e1b156
Mega-Barrel <saurabhjoshi7869@gmail.com>
2021-07-12 20:04:34 +0530

    Created Env variables for database connection

--- db.js
+++ db.js
@@ -1,21 +1,13 @@
 // File for connecting to postgreSQL database
 const Pool = require("pg").Pool;
 
-// Database Crendentials
-
-const UserName = process.env.user;
-const DBPassword = process.env.password;
-const DB = process.env.database;
-const Host = process.env.host;
-const Port = process.env.port;
-
 // Connecting to DB
 const pool = new Pool({
-    user: UserName,
-    password: DBPassword,
-    database: DB,
-    host: Host,
-    port: Port
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    host: process.env.host,
+    port: process.env.port
 });
-
+// Exporting the pool Postgresql connection
 module.exports = pool;
