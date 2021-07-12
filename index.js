44a5f3094d01fa99b9356abcf7bb6be993736bc2
Mega-Barrel <saurabhjoshi7869@gmail.com>
2021-07-12 20:06:29 +0530

    Added code for Login Details for users

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const pool = require("./db")
+const pool = require("./db");
 
 app.use(express.json()) // => req.body
 
 // ROUTES //
 
+// Checking if the server is listening to our queries
+app.listen(5000, () => {
+    console.log("Server is Listening on Port 5000");
+});
+
 // Login Data
+app.post('/userRegister', async (req, res) => {
+    try {
+        console.log("test");
+        const {
+            firstname,
+            lastname,
+            username,
+            password
+        } = req.body;
+        const RegisterUsers = await pool.query("INSERT INTO users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4) RETURNING *",
+            [firstname, lastname, username, password]
+        );
+        console.log("Passed this line");
+        res.json(RegisterUsers.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
 
 // Register Data
 
